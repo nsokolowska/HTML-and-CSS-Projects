dd976759398d85186f23b9969635810cda9f44fd
nsokolowska <111557072+nsokolowska@users.noreply.github.com>
2022-09-19 12:18:15 +0100

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+alert("Hello, my name is Natalia!");
+
+function Function_1(){
+var String = "I love cookies, and you?";
+var result = String.fontcolor("blue");
+document.getElementByID("Cookie").innerHTML =
+result;
+}
