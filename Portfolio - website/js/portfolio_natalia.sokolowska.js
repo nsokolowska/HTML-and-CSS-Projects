7c0152b03c8419f1ebbb80e8d05acc56d1d27ac0
nsokolowska <natalia.sokolowska12@gmail.com>
2022-12-30 19:58:34 +0000

    Create portfolio_natalia.sokolowska.js

---
+++ Portfolio - website/js/portfolio_natalia.sokolowska.js
@@ -0,0 +1,44 @@
+// FORM FUNCTIONS//
+
+function openForm() {
+    document.getElementById("Form").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("Form").style.display = "none";
+}
+
+
+//SHOWSLIDE//
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var slides = document.getElementsByClassName("Slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < slides.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active"
+}
+
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
