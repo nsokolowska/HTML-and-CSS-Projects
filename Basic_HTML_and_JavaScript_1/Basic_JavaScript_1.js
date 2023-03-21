398e63a73c8555518f8c674136a2288721d9013d
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-21 15:00:16 +0000

    Delete Basic_JavaScript_1.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-
-alert("Hello, my name is Natalia!");
-
-function Function_1(){
-var String = "I love cookies, and you?";
-var result = String.fontcolor("blue");
-document.getElementByID("Cookie").innerHTML =
-result;
-}
